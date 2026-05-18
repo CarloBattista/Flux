7b218857c428d033ec0a671efbc4f4763b2dd775
CarloBattista <carlitobatti@gmail.com>
2026-05-19 00:13:27 +0200

    first commit

---
+++ src/routing/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  // General
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+    props: true,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const pageTitle = to.meta.title;
+  if (pageTitle) {
+    document.title = pageTitle;
+  } else {
+    document.title = 'All Converters';
+  }
+  next();
+});
+
+export default router;
